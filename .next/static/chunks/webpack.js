a73c34e6183df82e7b77fdbad9954aa0cf12cdd3
Malka-Daniel <daniel10malka@icloud.com>
2023-02-25 16:57:04 +0200

    css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "45f9b8f8947c2646"; }
+/******/ 		__webpack_require__.h = function() { return "f62937b88c0366ec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
